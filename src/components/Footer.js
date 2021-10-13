80256dabf4bf62bb1f21f0dba9e5196c7c48bdc4
abusedmedia <hello@fabiofranchino.com>
2021-10-13 17:18:56 +0200

    minor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@
 const Footer = () => {
   return (
     <footer>
-      <p>This tool uses <a href='https://www.presenta.cc'>PRESENTA Editor and API</a> for the template and image generation<br />and the public API of GitHub for the user informations.</p>
+      <p>This tool uses <a href='https://www.presenta.cc'>PRESENTA Editor and API</a> for the template and image generation
+        <br />The user informations are gathered using the public API of GitHub.
+      </p>
       <p className='small'>Review the <a href='https://www.presenta.cc/legal/privacy-policy'>Privacy Policy</a>, the <a href='https://www.presenta.cc/legal/cookie-policy'>Cookie Policy</a> and <a href='https://www.presenta.cc/legal/terms-of-service'>Terms Of Use</a> of PRESENTA applications and websites. </p>
     </footer>
   )
