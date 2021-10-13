91168b3feafa781d08f7f9a9b976ee0eb60d2944
abusedmedia <hello@fabiofranchino.com>
2021-10-13 20:37:17 +0200

    build

--- dump.js
+++ dump.js
@@ -2,4 +2,4 @@ const fs = require('fs')
 
 const html = fs.readFileSync('./build/index.html', 'utf-8')
 const srcjs = JSON.stringify({ html })
-fs.writeFileSync('./src/functions/share/index.html.js', srcjs, 'utf-8')
+fs.writeFileSync('./src/functions/share/index.html.json', srcjs, 'utf-8')
